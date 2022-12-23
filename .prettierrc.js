02877f9e91ea3696ca1de521ee61764081dc4bd6
wupj <wdotl@outlook.com>
2022-12-23 16:30:14 +0800

    feat: 提交文件

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  tabWidth: 2,
+  semi: false,
+  printWidth: 100,
+  singleQuote: true,
+  quoteProps: 'consistent',
+  htmlWhitespaceSensitivity: 'strict',
+  vueIndentScriptAndStyle: true,
+  endOfLine: 'auto',
+}
